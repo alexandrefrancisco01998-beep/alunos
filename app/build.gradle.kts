43d415e2897be3c798e7abbaa48d4047fead2724
ALEXANDREFRANCISCO-ID <alexandrefrancisco01998@gmail.com>
2026-09-04 13:51:01 +0200

    Atualiza projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
-
+    // Substitua a linha do alias por esta:
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" // Use a mesma versão do Kotlin do projeto
     id("kotlin-kapt")
     id("kotlin-parcelize")
     id("com.google.gms.google-services")
