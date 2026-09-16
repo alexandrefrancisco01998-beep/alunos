423b76ce5cb62ad0b64f96b22194b3f9a2c03470
Alexandre Francisco <alexandrefrancisco01998@gmail.com>
2026-09-16 07:58:26 +0200

    Atualiza app

--- app/src/main/java/com/imobiliario/aluno/ui/perfil/PerfilScreen.kt
+++ app/src/main/java/com/imobiliario/aluno/ui/perfil/PerfilScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
